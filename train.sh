868244bc3cbababb7c66b3563ee0d907fdf7c12e
W3iii <xushuwei1122@gmail.com>
2026-04-20 12:18:57 +0800

    adding train.sh script

---
+++ train.sh
@@ -0,0 +1,23 @@
+mkdir -p ./models/lavt_ln_existBCE
+mkdir -p ./checkpoints/ln_existBCE_v2_weighted
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+    --model lavt \
+    --model_id lavt_ln_existBCE \
+    --bert_tokenizer dmis-lab/biobert-base-cased-v1.2 \
+    --ck_bert dmis-lab/biobert-base-cased-v1.2 \
+    --ln_dataset_root ../../groups/BME/LN_dataset_2D_vlm \
+    --batch-size 16 \
+    --lr 0.00005 \
+    --wd 1e-2 \
+    --swin_type base \
+    --pretrained_swin_weights ./pretrained_weights/swin_base_patch4_window12_384_22k.pth \
+    --window12 \
+    --epochs 100 \
+    --img_size 512 \
+    --workers 4 \
+    --pin_mem \
+    --output-dir ./checkpoints/ln_existBCE_v2_weighted \
+    --neg_ratio 1 \
+    --early_stop 999
+    2>&1 | tee ./models/lavt_ln_existBCE/output 2>&1
